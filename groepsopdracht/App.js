f9e640572ddfa667f2679fed01a808dd6282dbf9
LarsDMSoftware <larsdemaere@hotmail.com>
2020-12-09 20:04:26 +0100

    API call probleem

--- groepsopdracht/App.js
+++ groepsopdracht/App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createBottomTabNavigator}  from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import MapView, {Marker} from "react-native-maps";
 
 
 const Tab = createBottomTabNavigator();
 
 
-const Screen = ()=>{
-  return(
-    <View><Text>Hallo</Text></View>
+const Screen = ()=>
+{
+  //simpel map tonen
+  return (
+    <MapView style={styles.container}
+    region={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  />
   )
 }
 
+/*
+ <View>
+  <MapView style={styles.container} initialRegion={{latitude:50.878510004598574, longitude:4.378639994232532, latitudeDelta:0.12874,longitudeDelta:0.04888}}>
+    {loadTrein.length > 0 && loadTrein.map((markers, index) => (
+      
+          <Marker
+          key = {index}
+            coordinate=
+            {
+              {
+                latitude: markers.geometry.coordinates[1],
+                longitude: markers.geometry.coordinates[0],
+              }
+            }
+          />
+  ))}
+  </MapView>
+</View>
+*/
+
 export default function App() {
-  return (
+  return (  
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Map" component={Screen}/>
@@ -31,5 +61,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
+
+
+//API testing
+// API Call
+const [markers, setMarkers] = useState([]);
+
+const loadTrein = async() => 
+{
+  try
+  {
+    //https://api.jsonbin.io/b/5fd10ec7bef8b7699e577379
+    let response = await fetch('https://api.jsonbin.io/b/5fd10ec7bef8b7699e577379');
+    let json = await response.json();
+    setMarkers(json.features);
+  } catch (error) 
+    {
+      console.error(error);
+    }
+};
+
+ useEffect(() => 
+ {
+  LoadTrein();
+ }, []);
+ 
+
+  
